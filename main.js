0366bf2574f93a174fb27c1ad8d8a2048a3e7c60
Guido Lorenz <code@superquadratic.net>
2018-01-07 11:49:06 +0100

    Extract main function

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ const yaml = require('js-yaml')
 
 const WORKDAY = 8 * 60
 
-const computeBalance = (spans) => sum(spans.map(duration)) - WORKDAY
-const duration = ([start, end]) => end - start
+main()
 
-const timesheet = yaml.safeLoad(fs.readFileSync('timesheet.yml', 'utf8'))
-const balances = timesheet.map(day => computeBalance(day.spans))
-const dates = timesheet.map(day => day.date)
+function main() {
+  const timesheet = yaml.safeLoad(fs.readFileSync('timesheet.yml', 'utf8'))
+  const balances = timesheet.map(day => computeBalance(day.spans))
+  const dates = timesheet.map(day => day.date)
 
-console.log(zip(dates, balances))
-console.log(sum(balances))
+  console.log(zip(dates, balances))
+  console.log(sum(balances))
+}
+
+function computeBalance(spans) {
+  return sum(spans.map(duration)) - WORKDAY
+}
+
+function duration([start, end]) {
+  return end - start
+}
